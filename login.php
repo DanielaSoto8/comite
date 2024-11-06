e350100c549877d3be744abde579381bb034e751
Vico0810 <lauravblanco93@gmail.com>
2024-11-05 21:40:08 -0500

    se hace recapcha

--- login.php
+++ login.php
@@ -1,15 +1,15 @@
 <!-- <!-- <?php
-            // Inicia la sesión
-            //session_start();
+// Inicia la sesión
+//session_start();
 
-            // Verifica si la sesión está iniciada
-            //if (isset($_SESSION['id'])) {
-            // Si la sesión está iniciada, redirige al index.php
-            // header('Location: index.php');
-            // exit(); // Importante: asegúrate de salir del script después de redirigir
-            //}
-            //
-            ?>
+// Verifica si la sesión está iniciada
+//if (isset($_SESSION['id'])) {
+// Si la sesión está iniciada, redirige al index.php
+// header('Location: index.php');
+// exit(); // Importante: asegúrate de salir del script después de redirigir
+//}
+//
+?>
 <!DOCTYPE html>
 <html>
 
@@ -32,6 +32,7 @@
 </body>
 
 </html> -->
+ <!--
 <!DOCTYPE html>
 <html lang="en">
 
@@ -47,13 +48,13 @@
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
     <link href="css/ruang-admin.min.css" rel="stylesheet">
-    
+
 </head>
 
 <body class="bg-gradient-login">
-   <!-- Login Content-->
-    
-    <div class="container<!--  -->-login">
+    <!-- Login Content-->
+
+   <!-- <div class="container<!--  -login">
         <div class="row justify-content-center">
             <div class="col-xl-6 col-lg-12 col-md-9">
                 <div class="card shadow-sm my-5">
@@ -73,6 +74,92 @@
                                             <input type="password" name="contrasenia" class="form-control" required>
                                         </div>
                                         <div class="form-group">
+                                            <input type="submit" class="btn btn-success btn-block"
+                                                value="Iniciar Sesión">
+                                        </div>
+                                        <hr>
+                                        <a href="index.html" class="btn btn-google btn-block">
+                                            <i class="fab fa-google fa-fw"></i> Login with Google
+                                        </a>
+                                        <a href="index.html" class="btn btn-facebook btn-block">
+                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                                        </a>
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="font-weight-bold small" href="registrar.php">Create an Account!</a>
+                                    </div>
+                                    <div class="text-center">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--Login Content-->
+    <!-- <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="js/ruang-admin.min.js"></script>
+</body>
+
+</html> -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="img/logo/SENA.PNG" rel="icon">
+    <title>EduComitPro</title>
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="css/ruang-admin.min.css" rel="stylesheet">
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script> <!-- Script de reCAPTCHA -->
+
+    <style>
+        .recaptcha-center {
+            display: flex;
+            justify-content: center;
+        }
+    </style>
+
+</head>
+
+<body class="bg-gradient-login">
+    <!-- Login Content-->
+    <div class="container-login">
+        <div class="row justify-content-center">
+            <div class="col-xl-6 col-lg-12 col-md-9">
+                <div class="card shadow-sm my-5">
+                    <div class="card-body p-0">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="login-form">
+                                    <div class="text-center">
+                                        <img class="imagen" src="img/sena.png" alt="">
+                                        <h1 class="h4 text-gray-900 mb-4">Login</h1>
+                                    </div>
+                                    <form class="user" method="post" action="op_login.php">
+                                        <div class="form-group">
+                                            <input type="text" name="usuario" class="form-control" required placeholder="Usuario">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" name="contrasenia" class="form-control" required placeholder="Contraseña">
+                                        </div>
+                                        <!-- Centrado de reCAPTCHA -->
+                                        <div class="form-group recaptcha-center">
+                                            <div class="g-recaptcha" data-sitekey="6LdiH3IqAAAAAFRFc8l3U6WPKozYgEB1Mb6onLHb"></div>
+                                        </div>
+                                        <div class="form-group">
                                             <input type="submit" class="btn btn-success btn-block" value="Iniciar Sesión">
                                         </div>
                                         <hr>
@@ -85,7 +172,7 @@
                                     </form>
                                     <hr>
                                     <div class="text-center">
-                                        <a class="font-weight-bold small" href="register.html">Create an Account!</a>
+                                        <a class="font-weight-bold small" href="registrar.php">Create an Account!</a>
                                     </div>
                                     <div class="text-center">
                                     </div>
